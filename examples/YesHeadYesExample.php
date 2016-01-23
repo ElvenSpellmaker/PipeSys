a3d07a60843658b4df8c55061d8d8519222da651
ElvenSpellmaker <Jack@elvenspellmaker.co.uk>
2016-01-23 16:12:51 +0000

    Implements the ability to run `x` times.
     * Implements the ability to run `x` times.
     * Implements the ability to keep running the scheduler as many times as you
       like. (Excellent for keeping track of long-running or infinite-running command chains.)
     * Implements and closes #14.

---
+++ examples/YesHeadYesExample.php
@@ -0,0 +1,56 @@
+#!/usr/bin/env php
+<?php
+/**
+ * @title    Tests two infintie commands sandwiched by a terminating command.
+ * @command  yes | head | yes
+ * @expected y\ny\ny\ny\ny\ny\ny\ny\ny\ny\n
+ */
+
+error_reporting(E_ALL);
+
+include __DIR__ . '/../vendor/autoload.php';
+
+use ElvenSpellmaker\PipeSys as PS;
+use ElvenSpellmaker\PipeSys\Command as Command;
+
+$c = new PS\Scheduler(new Command\StandardConnector);
+$c->addCommand(new Command\Yes);
+$c->addCommand(new Command\Head(1));
+$c->addCommand(new Command\Yes);
+
+$output = '';
+
+if ($count = count($c->run(3)) !== 3)
+{
+	$output .= "Expecting 3 elements, received $count\n";
+}
+
+$expected = [
+	0 => 'ElvenSpellmaker\PipeSys\Command\Yes',
+	2 => 'ElvenSpellmaker\PipeSys\Command\Yes',
+];
+$received = array_map('get_class', $c->run(1));
+
+if ($received !== $expected)
+{
+	echo '<pre>';
+	var_dump($expected, $received);
+	exit;
+
+	$output .= "Expecting the head to be missing!\n";
+}
+
+$expected = [2 => 'ElvenSpellmaker\PipeSys\Command\Yes'];
+$received = array_map('get_class', $c->run(1));
+
+if ($received !== $expected)
+{
+	$output .= "Expecting the last yes to be present only!\n";
+}
+
+if (! count($c->run(5)))
+{
+	$output .= "Command has terminated sometime before 10 runs!\n";
+}
+
+echo $output;
